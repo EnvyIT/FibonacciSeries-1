7ff868eab50822dd9c38f2adb6df4d8b009c3a1d
loosercorp <richard.dujmovic@liwest.at>
2020-02-28 17:04:40 +0100

    first down

---
+++ src/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+//r7
+//refs
+
+namespace FibonacciSeries {
+  class Program {
+    static void Main(string[] args) {
+	  int count = 5;
+	  
+	  if(args.Length ==1){
+		  int.TryParse(args[0], out count);
+	  }
+		
+      int[] fibonacci = CreateFibonacciSeries(count);
+
+      for (int i = 0; i < fibonacci.Length; i++) {
+        Console.Out.Write(fibonacci[i]);
+        Console.Out.Write(" ");
+      }
+      Console.Out.WriteLine();
+    }
+
+    static int[] CreateFibonacciSeries(int count) {
+      int[] fibonacci = new int[count];
+      fibonacci[0] = 0;
+      fibonacci[1] = 1;
+
+      for (int i = 2; i < count; i++) {
+        fibonacci[i] = fibonacci[i - 1] + fibonacci[i - 2];
+      }
+      return fibonacci;
+    }
+  }
+}
